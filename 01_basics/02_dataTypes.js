22c4457c76c6d56723d00d3eaf76da9266bec692
lopsang-hyolmopa <lopsang.lama.comparewheels@gmail.com>
2024-03-05 16:11:56 +0545

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict" // treat all JS code as newer version
+
+// alert("Hello") // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should  be high
+
+console.log("Lopsang")
+
+
+let name = "lopsang"
+let age = 18
+let isLoggedIn = false
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value (type is object in js)
+// undefined => value not assigned to variable
+// symbol => unqiue
+
+
+// object
+
+console.log(typeof undefined);
