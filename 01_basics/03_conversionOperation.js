247e7448f18960889e8ec5d65b1d51c672cb2067
lopsang-hyolmopa <lopsang.lama.comparewheels@gmail.com>
2024-03-05 16:57:50 +0545

    conversion of datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -23,5 +23,35 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ***************** Operations ***************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// Basic Numeric Operations
+// console.log(2+2); => 4
+// console.log(2-2); => 0
+// console.log(2*2); => 4
+// console.log(2**2); => 4 (power)
+// console.log(2/2); => 1
+// console.log(2%2); => 
+
+let str1 = "hello"
+let str2 = " lopsang"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2); // 12
+// console.log(1 + "2"); // 12
+// console.log("1" + 2 + 2); // 122 (if there is string in first, it treates all as strings afterwards)
+// console.log(1 + 2 + "2"); // 32 (if there is string in last, it covert first and at last provides string value)
+
+
+console.log(+true); // 1
+console.log(+""); // 0
+
